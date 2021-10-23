9587ffd63e9f0feb3d04bec4e2a1a6f9da5c401a
Yousuf <youssefmesalm@yahoo.com>
2021-10-23 12:18:26 +0000

    update

--- src/components/BookChanger.js
+++ src/components/BookChanger.js
@@ -4,41 +4,52 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class BookChanger extends Component {
- state={
-   value:this.props.book.shelf===undefined?"none":this.props.book.shelf
- }
+  state = {
+    value: "",
+  };
   HandleChange = (event) => {
     const { book } = this.props;
-    this.setState({value:event.target.value})
+    this.setState({ value: event.target.value });
     this.props.dispatch(Change_Shelf(book, event.target.value));
-  
   };
 
   render() {
-    const { value } = this.state;
+    const { book, current, wanttored, read } = this.props;
+    let c = current.find((obj) => obj.id === book.id);
+    let w = wanttored.find((obj) => obj.id === book.id);
+    let r = read.find((obj) => obj.id === book.id);
+
+    let shelf = book.shelf
+      ? book.shelf
+      : c
+      ? c.shelf
+      : r
+      ? r.shelf
+      : w
+      ? w.shelf
+      : "none";
+    console.log(shelf);
     return (
       <div>
         <div className="book-shelf-changer">
-          <select
-            value={value}
-            onChange={(e) => this.HandleChange(e)}
-          >
+          <select value={shelf} onChange={(e) => this.HandleChange(e)}>
             <option value="move" disabled>
               Move to...
             </option>
-            <option  value="currentlyReading">
-              Currently Reading
-            </option>
+            <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
             <option value="read">Read</option>
             <option value="none">None</option>
           </select>
-          
         </div>
       </div>
     );
   }
 }
 
-
-export default connect()(BookChanger);
+const mapStateToProps = (state) => ({
+  current: state.BooksReducer.currentbooks,
+  wanttored: state.BooksReducer.wanttoreadbooks,
+  read: state.BooksReducer.readbooks,
+});
+export default connect(mapStateToProps)(BookChanger);
